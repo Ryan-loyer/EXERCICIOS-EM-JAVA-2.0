0310f20d71708bf6f4e88f8dd319d38b0f3f9796
Pablo Ryan <pabloryanlopes@gmail.com>
2025-09-21 17:44:50 -0300

    feat
    1 a 100 ordem decrescente

--- listapoo2/main.java
+++ listapoo2/main.java
@@ -3,8 +3,13 @@ package listapoo2;
 public class main {
     public static void main(String[] args) {
         questao1 q1 = new questao1();
-        q1.run();
+        //q1.run();
         questao2 q2 = new questao2();
         //q2.run();
+        questao3 q3 = new questao3();
+        //q3.run();
+        questao4 q4 = new questao4();
+        q4.run();
+        
     }
 }
---
+++ listapoo2/questao4.java
@@ -0,0 +1,14 @@
+package listapoo2;
+import java.util.Scanner;
+
+public class questao4 {
+    public void run(){
+         Scanner scanner = new Scanner(System.in);
+        
+         for(int cont = 100; cont>=1; cont--){
+            System.out.println(cont);
+         }
+
+         scanner.close();
+    }
+}
